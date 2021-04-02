7f30fc01cf3d1b7ccc5b19e60533b382fff4ae16
Marcus Maia <marcusmaialima@gmail.com>
2021-04-02 18:08:56 -0300

    chore: configuração do saga e rootSaga no projeto

--- src/config/store.js
+++ src/config/store.js
@@ -1,15 +1,22 @@
 import { configureStore } from '@reduxjs/toolkit'
+import createSagaMiddleware from 'redux-saga'
+import { rootSaga } from './sagas'
 
 const middlewares = []
+const sagaMiddleware = createSagaMiddleware()
 
 if (process.env.NODE_ENV === `development`) {
   const { logger } = require(`redux-logger`)
   middlewares.push(logger)
 }
 
+middlewares.push(sagaMiddleware)
+
 const store = configureStore({
   reducer: {},
   middleware: middlewares
 })
+
+sagaMiddleware.run(rootSaga)
 
 export default store
