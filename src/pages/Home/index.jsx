3079f3fcc9d8e41091faa18e429753aa0ab0299b
Marcus Maia <marcusmaialima@gmail.com>
2021-04-02 22:36:37 -0300

    feat: lading page já funcionando a rota de start

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+import Button from 'components/Button'
+import Logo from 'assets/images/pokemonLogo.png'
 
 import * as S from './styled'
 
 const HomePage = () => (
   <S.HomeWrapper>
-    <h1>Home</h1>
+    <S.LogoPokemon src={Logo} />
+    <Link to="/map">
+      <Button text="Start" />
+    </Link>
   </S.HomeWrapper>
 )
 
--- src/pages/Home/styled.js
+++ src/pages/Home/styled.js
@@ -1,3 +1,23 @@
 import styled from 'styled-components'
 
-export const HomeWrapper = styled.div``
+export const HomeWrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background: linear-gradient(
+    90deg,
+    rgba(0, 214, 143, 1) 35%,
+    rgba(0, 214, 143, 0.48) 98%
+  );
+`
+
+export const LogoPokemon = styled.img`
+  width: auto;
+  max-width: 270px;
+  height: auto;
+  max-height: 90px;
+  margin-bottom: 30px;
+`
